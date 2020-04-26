3cbcffa798b276dfbdf15debc348bba87d0fd14b
domtaylor <dominick.taylor@gmail.com>
2020-04-26 12:47:08 +0100

    final commit images rendering, social icons added

--- src/date.js
+++ src/date.js
@@ -2,16 +2,16 @@ var now = new Date().getTime(); //Return the number of milliseconds since 1970/0
 
 var epochTimeApr30_1201am = 1588204801; // number of milliseconds since 1970/01/01 at Jul 15_11:59:59pm. See http://www.epochconverter.com/.
 
-var timeTillChange = epochTimeApr30_1201am - now;
+var timeTillChange = now - epochTimeApr30_1201am;
 
 function changeSubText()
 {
     if(epochTimeApr30_1201am <= now) 
     {
-        document.getElementById('subtext').innerHTML = 'Summer 2020 ';
+        document.getElementById('li').innerHTML = 'Summer 2020 Intake ';
     }
     else {
-        document.getElementById('subtext').innerHTML = 'Winter 2020 '; 
+        document.getElementById('li').innerHTML = 'Winter 2020 Intake'; 
     }
 }
 
